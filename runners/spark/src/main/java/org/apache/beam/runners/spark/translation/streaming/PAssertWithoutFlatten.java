fb831bed9ae093bd4556d1439e47358491c0061a
Sela <ansela@paypal.com>
2017-02-09 13:38:30 +0200

    Revert flatten to master implementation.

---
+++ runners/spark/src/main/java/org/apache/beam/runners/spark/translation/streaming/PAssertWithoutFlatten.java
@@ -0,0 +1,50 @@
+package org.apache.beam.runners.spark.translation.streaming;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.isIn;
+
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.transforms.Aggregator;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.Sum;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PDone;
+
+
+/**
+ * Created by ansela on 2/9/17.
+ */
+public class PAssertWithoutFlatten extends PTransform<PCollection<Long>, PDone> {
+
+  @Override
+  public PDone expand(PCollection<Long> input) {
+    input.apply(ParDo.of(new AssertDoFn(4L, 5L)));
+    return PDone.in(input.getPipeline());
+  }
+
+  private static class AssertDoFn extends DoFn<Long, Void> {
+    private final Aggregator<Integer, Integer> success =
+            createAggregator(PAssert.SUCCESS_COUNTER, Sum.ofIntegers());
+    private final Aggregator<Integer, Integer> failure =
+            createAggregator(PAssert.FAILURE_COUNTER, Sum.ofIntegers());
+    private final Long[] possibleResults;
+
+    AssertDoFn(Long... possibleResults) {
+      this.possibleResults = possibleResults;
+    }
+
+    @ProcessElement
+    public void processElement(ProcessContext c) throws Exception {
+      try {
+        System.out.println("Assert output: " + c.element());
+        assertThat(c.element(), isIn(possibleResults));
+        success.addValue(1);
+      } catch (Throwable t) {
+        failure.addValue(1);
+        throw t;
+      }
+    }
+  }
+}
--- runners/spark/src/main/java/org/apache/beam/runners/spark/translation/streaming/StreamingTransformTranslator.java
+++ runners/spark/src/main/java/org/apache/beam/runners/spark/translation/streaming/StreamingTransformTranslator.java
@@ -17,6 +17,7 @@
  */
 package org.apache.beam.runners.spark.translation.streaming;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
 import static org.apache.beam.runners.spark.translation.TranslationUtils.rejectStateAndTimers;
 
@@ -29,7 +30,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
-import java.util.concurrent.LinkedBlockingQueue;
 import org.apache.beam.runners.spark.aggregators.NamedAggregators;
 import org.apache.beam.runners.spark.aggregators.SparkAggregators;
 import org.apache.beam.runners.spark.coders.CoderHelpers;
@@ -73,8 +73,8 @@ import org.apache.beam.sdk.util.WindowedValue;
 import org.apache.beam.sdk.util.WindowingStrategy;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
-import org.apache.beam.sdk.values.PCollectionList;
 import org.apache.beam.sdk.values.PCollectionTuple;
+import org.apache.beam.sdk.values.TaggedPValue;
 import org.apache.beam.sdk.values.TupleTag;
 import org.apache.spark.Accumulator;
 import org.apache.spark.api.java.JavaPairRDD;
@@ -136,27 +136,44 @@ final class StreamingTransformTranslator {
       @SuppressWarnings("unchecked")
       @Override
       public void evaluate(Flatten.FlattenPCollectionList<T> transform, EvaluationContext context) {
-        PCollectionList<T> pcs = context.getInput(transform);
+        List<TaggedPValue> pcs = context.getInput(transform).expand();
         // since this is a streaming pipeline, at least one of the PCollections to "flatten" are
         // unbounded, meaning it represents a DStream.
         // So we could end up with an unbounded unified DStream.
+        final List<JavaRDD<WindowedValue<T>>> rdds = new ArrayList<>();
         final List<JavaDStream<WindowedValue<T>>> dStreams = new ArrayList<>();
-        for (PCollection<T> pcol : pcs.getAll()) {
-         Dataset dataset = context.borrowDataset(pcol);
+        for (TaggedPValue pv : pcs) {
+          checkArgument(
+              pv.getValue() instanceof PCollection,
+              "Flatten had non-PCollection value in input: %s of type %s",
+              pv.getValue(),
+              pv.getValue().getClass().getSimpleName());
+          PCollection<T> pcol = (PCollection<T>) pv.getValue();
+          Dataset dataset = context.borrowDataset(pcol);
           if (dataset instanceof UnboundedDataset) {
             dStreams.add(((UnboundedDataset<T>) dataset).getDStream());
           } else {
-            // create a single RDD stream.
-            Queue<JavaRDD<WindowedValue<T>>> q = new LinkedBlockingQueue<>();
-            q.offer(((BoundedDataset) dataset).getRDD());
-            JavaDStream<WindowedValue<T>> dStream = context.getStreamingContext().queueStream(q);
-            dStreams.add(dStream);
+            rdds.add(((BoundedDataset<T>) dataset).getRDD());
           }
         }
         // start by unifying streams into a single stream.
         JavaDStream<WindowedValue<T>> unifiedStreams =
             context.getStreamingContext().union(dStreams.remove(0), dStreams);
-        context.putDataset(transform, new UnboundedDataset<>(unifiedStreams));
+        // now unify in RDDs.
+        if (rdds.size() > 0) {
+          JavaDStream<WindowedValue<T>> joined =
+              unifiedStreams.transform(
+                  new Function<JavaRDD<WindowedValue<T>>, JavaRDD<WindowedValue<T>>>() {
+                    @Override
+                    public JavaRDD<WindowedValue<T>> call(JavaRDD<WindowedValue<T>> streamRdd)
+                        throws Exception {
+                      return new JavaSparkContext(streamRdd.context()).union(streamRdd, rdds);
+                    }
+                  });
+          context.putDataset(transform, new UnboundedDataset<>(joined));
+        } else {
+          context.putDataset(transform, new UnboundedDataset<>(unifiedStreams));
+        }
       }
     };
   }
